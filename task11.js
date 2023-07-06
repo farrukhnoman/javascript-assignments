cb2bf332097f5be57462b609b56495912b8fe075
farrukhnoman <137686990+farrukhnoman@users.noreply.github.com>
2023-07-06 15:11:39 +0500

    Update task11.js

--- task11.js
+++ task11.js
@@ -1,6 +1,17 @@
+function cToF(celsius) 
+{
+  var cTemp = celsius;
+  var cToFahr = cTemp * 9 / 5 + 32;
+  var message = cTemp+'\xB0C is ' + cToFahr + ' \xB0F.';
+    console.log(message);
+}
 
-var f=25/77;
-var tempCent=(f-32*5/9)
-var centigrade=1;
-
-
+function fToC(fahrenheit) 
+{
+  var fTemp = fahrenheit;
+  var fToCel = (fTemp - 32) * 5 / 9;
+  var message = fTemp+'\xB0F is ' + fToCel + '\xB0C.';
+    console.log(message);
+} 
+cToF(25);
+fToC(70);
